2c36fbc90ca6f929bf79ca533cd97a25dd0ff6ea
Wenqian Zhang <zhangwenqian6915@gmail.com>
2025-06-19 23:21:02 +1000

    complite base rs

--- tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/zhangwenqian/UNSW/pivoter/tests
-# Build directory: /Users/zhangwenqian/UNSW/pivoter/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(BronKerboschTest "/Users/zhangwenqian/UNSW/pivoter/tests/test_bronkerbosch")
-set_tests_properties(BronKerboschTest PROPERTIES  _BACKTRACE_TRIPLES "/Users/zhangwenqian/UNSW/pivoter/tests/CMakeLists.txt;59;add_test;/Users/zhangwenqian/UNSW/pivoter/tests/CMakeLists.txt;0;")
